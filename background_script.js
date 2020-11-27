23fb01362ea3d1a9051af63935905fb04d41889f
bilder <514059172@163.com>
2020-11-27 15:17:07 +0800

    use the test page instead the broswerAction pop up

--- background_script.js
+++ background_script.js
@@ -1,1 +1,16 @@
 // Put all the javascript code here, that you want to execute in background.
+function onCreated(tab) {
+  console.log(`success to open tab ${tab.id}`);
+}
+
+function onError(e) {
+  console.log(`creat new tab failed with error:${e}`);
+}
+browser.browserAction.onClicked.addListener(creatTabs);
+function creatTabs() {
+  const creatingTab = browser.tabs.create({
+    active: true,
+    url: `/testPage/index.html`,
+  });
+  creatingTab.then(onCreated,onError);
+}
